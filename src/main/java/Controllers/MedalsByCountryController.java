0b8891af0effe42486ba12d705c2329310a99a1d
Preston-Beck <pgbeck@lakeheadu.ca>
2021-10-18 20:06:34 -0400

    Added RadioButton Change Listeners.

--- src/main/java/Controllers/MedalsByCountryController.java
+++ src/main/java/Controllers/MedalsByCountryController.java
@@ -1,12 +1,14 @@
 package Controllers;
 
 import Utilities.DBUtilities;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 
 import java.net.URL;
@@ -47,4 +49,32 @@ public class MedalsByCountryController implements Initializable {
         countryAxis.setLabel("Countries");
         medalsAxis.setLabel("Medals");
     }
+
+    public void changed(ObservableValue<? extends Toggle> observed,
+                        Toggle oldValue, Toggle newValue)
+        {
+
+            RadioButton radioButton = (RadioButton)graphControls.getSelectedToggle();
+
+            if (radioButton == goldMedalRB) {
+                barChart.getData().addAll(DBUtilities.getGoldMedalsByCountry());
+                //Change title for chart
+                barChart.setTitle("Olympic Gold Medals");
+            }
+            else if (radioButton == silverMedalRB) {
+                barChart.getData().addAll(DBUtilities.getSilverMedalsByCountry());
+                //Change title for chart
+                barChart.setTitle("Olympic Silver Medals");
+            }
+            else if (radioButton == bronzeMedalRB) {
+                barChart.getData().addAll(DBUtilities.getBronzeMedalsByCountry());
+                //Change title for chart
+                barChart.setTitle("Olympic Bronze Medals");
+            }
+            else if (radioButton == totalMedalsRB) {
+                barChart.getData().addAll(DBUtilities.getTotalMedalsByCountry());
+                //Change title for chart
+                barChart.setTitle("Olympic Total Medals");
+            }
+        }
 }
